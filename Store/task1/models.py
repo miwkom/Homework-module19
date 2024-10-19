7f5b89bd03d5d25b039128b24af809e0fde7f9b5
miwkom <166976262+miwkom@users.noreply.github.com>
2024-10-19 04:18:52 +0300

    Add files via upload

--- Store/task1/models.py
+++ Store/task1/models.py
@@ -3,14 +3,20 @@ from django.db import models
 
 class Buyer(models.Model):
     username = models.CharField(max_length=50)
-    balance = models.DecimalField(max_digits=5, decimal_places=2)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.username
 
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_digits=5, decimal_places=2)
-    size = models.DecimalField(max_digits=5, decimal_places=2)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='buyer')
+
+    def __str__(self):
+        return self.title
